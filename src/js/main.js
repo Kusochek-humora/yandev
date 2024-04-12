8cac3c78ed130a4ad297e93020e2a8f2fa9db96e
Kusochek-humora <69989303+Kusochek-humora@users.noreply.github.com>
2024-04-13 02:31:36 +0600

    portfolio mobile + desctkop

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function () {
+
+    // БУРГЕР МЕНЮ
     const header = document.getElementById('header'),
         menuOpenBtn = document.getElementById('header__burger'),
         menuCloseBtn = document.getElementById('menu__burger'),
@@ -21,8 +23,42 @@ document.addEventListener('DOMContentLoaded', function () {
     menuOpenBtn.addEventListener('click', openMenuHandler);
 
 
+    // ТАБЫ 
+    const cardsArray = document.querySelectorAll('.portfolio__card');
+    const tabsContainer = document.getElementById('tabs');
+    function tabsHanlder(e) {
+        const target = e.target;
+        const tabArray = tabsContainer.querySelectorAll('.portfolio__tab-btn');
+
+
+
+        if (target && target.classList.contains('portfolio__tab-btn')) {
+            tabArray.forEach(item => {
+                item.classList.remove('active');
+            });
+            target.classList.add('active');
+            const tabName = target.getAttribute("data-tab");
+            filterBlocks(tabName);
+        };
+
+    };
+
+    function filterBlocks(tabName) {
+        cardsArray.forEach(block => {
+            const blockTags = block.getAttribute("data-tags").split(",");
+            if (tabName === "all" || blockTags.includes(tabName)) {
+                block.style.display = "block";
+            } else {
+                block.style.display = "none";
+            }
+        });
+    }
+    tabsContainer.addEventListener('click', tabsHanlder);
+
+
+    // ПЕЧАТАЮЩИЙСЯ ТЕКСТ
     const options = {
-        strings: ['web-designer','front-end developer'],
+        strings: ['web-designer', 'front-end developer'],
         typeSpeed: 100,       // Установите желаемую скорость набора символов
         backSpeed: 75,       // Скорость стирания текста
         startDelay: 75,     // Задержка перед началом анимации
